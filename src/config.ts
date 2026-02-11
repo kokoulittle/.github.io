96318bbcf7f64f8f9957dd16a57b65e6e2219ff6
Jacques WEMEGAN <104101916+kokoulittle@users.noreply.github.com>
2026-02-11 14:22:38 +0000

    Refactor skills array to structured object format
    Updated skills structure to include detailed categories for languages, libraries, and data visualization tools.

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,12 @@ export const siteConfig = {
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
-  skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
+  skills: [{Languages: ["Python", "SQL", "VBA"],
+            Libraires: ["Pandas", "NumPy", "Matplotlib", "Scikit-Learn"],
+            DataViz: ["Plotly", "Excel", "Matplotlib", "Seaborn"] 
+           }
+          },
+           ]
   projects: [
     {
       name: "AI Dev Roundup Newsletter",
